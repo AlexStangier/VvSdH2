707efc4059d46d87476565ffbf1bf5f72a33dc78
AlexStan <Alex.stangier@gmail.com>
2020-05-15 21:22:59 +0200

    made easier asnyc call to retreive user info from db w/o locking

--- Application/UserController.cs
+++ Application/UserController.cs
@@ -1,55 +1,48 @@
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Core;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace Application
 {
     public class UserController
     {
-        // TODO add the timeout functionality
-
-        private object _userLock = new object();
         private User _loggedInAs;
 
         public async Task<bool> login(DateTime timestampLogin, User user)
         {
-            lock (_userLock)
+            var context = new ReservationContext();
+            var foundUser = await context.Users.FirstOrDefaultAsync(x => x.Username.Equals(user.Username));
+
+            if (foundUser == null)
             {
-                var rc = new ReservationContext();
-                var foundUser = rc.Users.Find(user.Username);
-
-                if(foundUser == null)
-                {
-                    // No user with that username in DB
-                    return false;
-                }
-
-                if(foundUser.Password != user.Password)
-                {
-                    // Password doesn't match
-                    return false;
-                }
-
-                _loggedInAs = user;
-                return true;
+                // No user with that username in DB
+                return false;
             }
+
+            if (foundUser.Password != user.Password)
+            {
+                // Password doesn't match
+                return false;
+            }
+
+            _loggedInAs = user;
+            return true;
         }
 
         public async Task<bool> logout()
         {
-            lock (_userLock)
+            if (_loggedInAs == null)
             {
-                if (_loggedInAs == null)
-                {
-                    // can't log out because the user is not logged in.
-                    return false;
-                }
-
-                _loggedInAs = null;
-                return true;
+                // can't log out because the user is not logged in.
+                return false;
             }
+
+            _loggedInAs = null;
+            return true;
         }
     }
 }
