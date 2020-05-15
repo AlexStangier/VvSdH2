9c2ff1fc56d85e06141436db7fc287045de34744
Luk-T <ltartler@hs-offenburg.de>
2020-05-15 20:16:14 +0200

    First implementation of UserController

--- Application/UserController.cs
+++ Application/UserController.cs
@@ -1,19 +1,55 @@
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Core;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace Application
 {
     public class UserController
     {
+        // TODO add the timeout functionality
+
+        private object _userLock = new object();
+        private User _loggedInAs;
+
         public async Task<bool> login(DateTime timestampLogin, User user)
         {
-            throw new NotImplementedException();
+            lock (_userLock)
+            {
+                var rc = new ReservationContext();
+                var foundUser = rc.Users.Find(user.Username);
+
+                if(foundUser == null)
+                {
+                    // No user with that username in DB
+                    return false;
+                }
+
+                if(foundUser.Password != user.Password)
+                {
+                    // Password doesn't match
+                    return false;
+                }
+
+                _loggedInAs = user;
+                return true;
+            }
         }
 
         public async Task<bool> logout()
         {
-            throw new NotImplementedException();
+            lock (_userLock)
+            {
+                if (_loggedInAs == null)
+                {
+                    // can't log out because the user is not logged in.
+                    return false;
+                }
+
+                _loggedInAs = null;
+                return true;
+            }
         }
     }
 }
