eaaf75c7730f8aed60d185db69cb319715ee9642
Daniel <Daniel@DESKTOP-TF8814U>
2020-05-22 19:54:04 +0200

    added Holyday table

---
+++ Core/Holyday.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core
+{
+    public class Holyday
+    {
+        [Key]
+        public DateTime day { get; set; }
+        public string reason { get; set; }
+    }
+}
