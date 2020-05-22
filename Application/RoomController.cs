388718ec41896b1170445c20e58c1b925348f71d
Luk-T <ltartler@hs-offenburg.de>
2020-05-22 20:20:51 +0200

    Added getFloor and filter methods. Signatures had to change.

--- Application/RoomController.cs
+++ Application/RoomController.cs
@@ -1,19 +1,47 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Core;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application
 {
     public class RoomController
     {
-        public async Task<List<Room>> getFloor()
+        /// <summary>
+        /// Gets all rooms of a floor in a given Building
+        /// </summary>
+        /// <param name="building">The building where the rooms should be</param>
+        /// <param name="floor">The floor of the rooms</param>
+        /// <returns></returns>
+        public async Task<List<Room>> getFloor(string building, int floor)
         {
-            throw new System.NotImplementedException();
+            var rc = new ReservationContext();
+            // Include notwendig?
+            var query = rc.Rooms.Where(x => x.Floor == floor)
+                                .Where(x => x.Building == building);
+
+            return query.ToList();
         }
 
-        public async Task<List<Room>> filter()
+        /// <summary>
+        /// Filters a list of Room by the size of the room and various attributes.
+        /// </summary>
+        /// <param name="rooms">The list of Rooms to be filtered</param>
+        /// <param name="size">The minimum Amount of Rooms. If null, no rooms will be filtered out</param>
+        /// <param name="attributes">The attributes that the room should have</param>
+        /// <returns></returns>
+        public async Task<List<Room>> filter(List<Room> rooms, int? size, Attribute attributes)
         {
-            throw new System.NotImplementedException();
+            int minSize = size ?? int.MinValue; 
+
+            var query = rooms.Where(x => x.Size >= minSize)
+                             .Where(x => !attributes.Computers || x.Attribute.Computers)
+                             .Where(x => !attributes.Jacks || x.Attribute.Jacks)
+                             .Where(x => !attributes.Presenter || x.Attribute.Presenter)
+                             .Where(x => !attributes.AirCon || x.Attribute.AirCon);
+
+            return query.ToList();
         }
 
         public async Task<Room> getCurrentStatus()
