4b39b1a20403f089d73c1023352a11c5899911e7
Andreas S <singer.andreas@gmx.de>
2020-05-25 20:25:50 +0200

    Add GUI

---
+++ AvaloniaUI/Views/LoginView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System;
+
+namespace AvaloniaUI.Views
+{
+    public class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            this.InitializeComponent();
+            this.DataContext =
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
