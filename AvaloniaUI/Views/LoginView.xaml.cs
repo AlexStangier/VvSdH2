c4ed79ca6efff3242aedc28ab8fe639c91eba14a
Andreas S <singer.andreas@gmx.de>
2020-05-29 21:39:04 +0200

    Login is working now

--- AvaloniaUI/Views/LoginView.xaml.cs
+++ AvaloniaUI/Views/LoginView.xaml.cs
@@ -1,6 +1,10 @@
-﻿using Avalonia;
+﻿using Application;
+using ApplicationShared;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using AvaloniaUI.Models;
 using System;
 
 namespace AvaloniaUI.Views
@@ -10,12 +14,30 @@ namespace AvaloniaUI.Views
         public LoginView()
         {
             this.InitializeComponent();
-            // this.DataContext =
+            this.DataContext = new LoginViewModel() { Username = "Benutzername", Password = "Passwort"};
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        public async void OnLoginClicked(object sender, RoutedEventArgs args)
+        {
+            var context = this.DataContext as LoginViewModel;
+            //context.Info = $"Hello {context.Username} with the Password {context.Password}";
+
+            IUser _user = new UserController();
+            var verified = await _user.Login(context.Username, context.Password);
+
+            if(verified)
+            {
+                context.Info = "Sie werden eingeloggt";
+            }
+            else
+            {
+                context.Info = "Der Benutzername oder Passwort ist falsch!";
+            }
         }
     }
 }
