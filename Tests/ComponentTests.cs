3b776f7a8ad11552f0c6fe72ad231078a44e6272
AlexStan <Alex.stangier@gmail.com>
2020-05-31 18:04:36 +0200

    added tests for getreservations

--- Tests/ComponentTests.cs
+++ Tests/ComponentTests.cs
@@ -38,7 +38,6 @@ namespace Tests
         [Test]
         public async Task TryReturnWholeFloor()
         {
-            SetUp();
             var rooms = await _room.GetFloor(1, "A");
 
             Assert.AreEqual(10, rooms.Count);
@@ -47,7 +46,6 @@ namespace Tests
         [Test]
         public async Task TryFilterRoomsBySize()
         {
-            SetUp();
             var rooms = await _room.GetFloor(1, "A");
             var filteredRooms = await _room.Filter(rooms, 50, new Core.Attribute());
 
@@ -57,7 +55,6 @@ namespace Tests
         [Test]
         public async Task TryStandartLogin()
         {
-            SetUp();
             var user = new User
             {
                 Username = "udo@hs-offenburg.de",
@@ -72,7 +69,6 @@ namespace Tests
         [Test]
         public async Task TryInvalidLoginWrongPassword()
         {
-            SetUp();
             var user = new User
             {
                 Username = "udo@hs-offenburg.de",
@@ -88,7 +84,6 @@ namespace Tests
         [Test]
         public async Task TryLoginWithoutCorrespondingUser()
         {
-            SetUp();
             var user = new User
             {
                 Username = "",
@@ -103,63 +98,54 @@ namespace Tests
         [Test]
         public async Task TryStandartLogout()
         {
-            SetUp();
             Assert.True(await _user.Logout("udo@hs-offenburg.de"));
         }
 
         [Test]
         public async Task TryLogoutWithInvalidCredentials()
         {
-            SetUp();
             Assert.False(await _user.Logout(""));
         }
 
         [Test]
         public async Task TryGetRoomByBuildingAndNumber()
         {
-            SetUp();
             Assert.NotNull(await _room.GetCurrentStatusForRoom(100, "A"));
         }
 
         [Test]
         public async Task TryGetRoomWithInvalidRoomNumber()
         {
-            SetUp();
             Assert.Null(await _room.GetCurrentStatusForRoom(0, "A"));
         }
 
         [Test]
         public async Task TryGetRoomWithInvalidBuilding()
         {
-            SetUp();
             Assert.Null(await _room.GetCurrentStatusForRoom(100, ""));
         }
 
         [Test]
         public async Task TryGetFloorByBuildingAndNumber()
         {
-            SetUp();
             Assert.NotNull(await _room.GetCurrentStatusForFloor("A", 1));
         }
 
         [Test]
         public void TryGetFloorWithInvalidRoomNumber()
         {
-            SetUp();
             Assert.AreEqual(0, _room.GetCurrentStatusForFloor("A", 0).Result.Count);
         }
 
         [Test]
         public void TryGetFloorWithInvalidBuilding()
         {
-            SetUp();
             Assert.AreEqual(0, _room.GetCurrentStatusForFloor("", 1).Result.Count);
         }
 
         [Test]
         public async Task TryCreateStandartReservation()
         {
-            SetUp();
             await using var context = new ReservationContext();
             var time = DateTime.Now;
             if (time.DayOfWeek.Equals(DayOfWeek.Sunday))
@@ -172,7 +158,6 @@ namespace Tests
         [Test]
         public async Task TryCreateReservationWithInvalidRoom()
         {
-            SetUp();
             var time = DateTime.Now;
             if (time.DayOfWeek == DayOfWeek.Sunday) time.AddDays(1);
             Assert.False(await _booking.CreateReservation(null, time, 90, dummyUser));
@@ -181,7 +166,6 @@ namespace Tests
         [Test]
         public async Task TryCreateReservationWithInvalidUser()
         {
-            SetUp();
             var time = DateTime.Now;
             if (time.DayOfWeek == DayOfWeek.Sunday) time.AddDays(1);
             Assert.False(await _booking.CreateReservation(dummyRoom, time, 90, null));
@@ -190,7 +174,6 @@ namespace Tests
         [Test]
         public async Task TryCreateReservationWithNegativeDuration()
         {
-            SetUp();
             await using var context = new ReservationContext();
             var time = DateTime.Now;
             if (time.DayOfWeek.Equals(DayOfWeek.Sunday))
@@ -213,7 +196,6 @@ namespace Tests
         [Test]
         public async Task TryCreateRerservation()
         {
-            SetUp();
             await using var context = new ReservationContext();
             var time = DateTime.Now;
             if (time.DayOfWeek.Equals(DayOfWeek.Sunday))
@@ -226,7 +208,6 @@ namespace Tests
         [Test]
         public async Task TryOverbookReservation()
         {
-            SetUp();
             await using var context = new ReservationContext();
             Assert.True(await _booking.CreateReservation(await context.Rooms.FindAsync(1), DateTime.Now.AddHours(25), 90, await context.Users.FindAsync("udo@hs-offenburg.de")));
         }
@@ -234,17 +215,30 @@ namespace Tests
         [Test]
         public async Task TrySundayRerservation()
         {
-            SetUp();
             await using var context = new ReservationContext();
             Assert.False(await _booking.CreateReservation(await context.Rooms.FindAsync(1), new DateTime(2020, 8, 30, 12, 0, 0), 90, await context.Users.FindAsync("alex@stud.hs-offenburg.de")));
         }
 
         [Test]
-        public async Task TryHolydayRerservation()
+        public async Task TryHolidayRerservation()
         {
-            SetUp();
             await using var context = new ReservationContext();
             Assert.False(await _booking.CreateReservation(await context.Rooms.FindAsync(1), new DateTime(2020, 12, 26, 12, 0, 0), 90, await context.Users.FindAsync("alex@stud.hs-offenburg.de")));
+        }
+        
+        [Test]
+        public async Task GetReservationsForSpecifiedUser()
+        {
+            await using var context = new ReservationContext();
+            Assert.NotNull(_booking.GetUserReservations(await context.Users.FindAsync("alex@stud.hs-offenburg.de")));
+        }
+        
+        [Test]
+        public async Task GetReservationsForInvalidUser()
+        {
+            await using var context = new ReservationContext();
+            var result = await _booking.GetUserReservations(dummyUser);
+            Assert.GreaterOrEqual(1,result?.Count ?? 0);
         }
     }
 }
