bdf55c4c134747a94dcb7edd61207fe5aacb3580
Luk-T <ltartler@hs-offenburg.de>
2020-06-08 10:18:33 +0200

    Added MailController

---
+++ Application/MailController.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationShared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class MailController : IMail
+    {
+        public Task SendConfirmationMail(string emailAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendOverbookingMail(string emailAddress)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
