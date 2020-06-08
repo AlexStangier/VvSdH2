5753315d334888e98995b650672a3acb8174a72f
Luk-T <ltartler@hs-offenburg.de>
2020-06-08 11:05:38 +0200

    First implementation of MailController

--- Application/MailController.cs
+++ Application/MailController.cs
@@ -1,6 +1,8 @@
 ﻿using ApplicationShared;
 using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,14 +10,35 @@ namespace Application
 {
     public class MailController : IMail
     {
-        public Task SendConfirmationMail(string emailAddress)
+        private const string mailName = "smtp.web.de";
+        private const string mailPW = "123d€fgh";
+        private const string mailServer = "smtp.web.de";
+        private const int port = 587;
+
+        // TODO Maybe customize mail content with information about the booking
+
+        public async Task SendConfirmationMail(string toAddress)
         {
-            throw new NotImplementedException();
+            await SendMail(toAddress,
+                           "Booking Succeeded!",
+                           "Room was reservated successfully. Thanks for using VvSdH!");
         }
 
-        public Task SendOverbookingMail(string emailAddress)
+        public async Task SendOverbookingMail(string toAddress)
         {
-            throw new NotImplementedException();
+            await SendMail(toAddress,
+                           "One of your reservations was overbooked",
+                           "Please be aware that one of your reservations has been overbooked. Please login to VvSdH for further information.");
+        }
+
+        public async Task SendMail(string toAddress, string subject, string message)
+        {
+            var smtp = new SmtpClient(mailServer, port);
+            smtp.Credentials = new NetworkCredential(mailName, mailPW);
+            smtp.EnableSsl = true;
+
+            var mail = new MailMessage(mailName, toAddress, subject, message);
+            await smtp.SendMailAsync(mail);
         }
     }
 }
