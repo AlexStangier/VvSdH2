b63c050fa6a736b0dbb357f82ddb62dd5b2074c2
LuksTrackmaniaCorner <ThrowawayAccount1@web.de>
2020-06-22 17:22:58 +0200

    Updated the GUI, when a new reservation is done

--- WPFGUI/ViewModels/ReservationViewModel.cs
+++ WPFGUI/ViewModels/ReservationViewModel.cs
@@ -374,6 +374,7 @@ namespace WPFGUI.ViewModels
                         {
                             MessageBox.Show("Reservierung war nicht erfolgreich. Versuchen Sie es nochmal.", "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
                         }
+                        UpdateRoomStatus();
                         break;
                     case MessageBoxResult.No:
                         break;
